27268caa0c963cd1ed3bda26ecb6a67d40320a18
Gantet Saturnin <saturnin.gantet@hotmail.Fr>
2021-01-15 15:44:57 +0100

    remove urgence2

--- controlleur.c
+++ controlleur.c
@@ -1,7 +1,19 @@
 #include "controlleur.h"
 
-void controlleur(float tabRadar[5], float pression, float temperature, bool urgence, float batterie, bool urgence2,
-            int position[2], float* vitesse, float *fréquanceEchantillon, bool *demande_secours, bool* active_capteur_supp)
+#define FREQ_BASE 2.0
+#define VITESSE_BASE 2.0
+
+void controlleur(float tabRadar[5], float pression, float temperature, bool urgence, float batterie,
+                 int position[2], float *vitesse, float *fréquanceEchantillon, bool *demande_secours, bool *active_capteur_supp)
 {
+    *fréquanceEchantillon = FREQ_BASE;
+    *vitesse = VITESSE_BASE;
+
+    bool secure = false;
+    for (int i = 0; i < 5; i++)
+        if (tabRadar[i] < 1.0)
+            secure = true;
+    if(secure)
+        *fréquanceEchantillon *= 2;
     
 }
--- controlleur.h
+++ controlleur.h
@@ -10,7 +10,7 @@
 #include <errno.h>
 
 
-void controlleur(float tabRadar[5], float pression, float temperature, bool urgence, float batterie, bool urgence2,
+void controlleur(float tabRadar[5], float pression, float temperature, bool urgence, float batterie,
             int position[2],float* vitesse, float* fréquanceEchantillon, bool* demande_secours, bool* active_capteur_supp);
 
 #endif
